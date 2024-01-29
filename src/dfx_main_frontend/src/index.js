e66ef338b463f12637429539837a69e17cd379a5
Ryan <rjl275@cornell.edu>
2024-01-28 23:58:20 -0500

    connect basic functional frontend to icp motoko backend

--- src/dfx_main_frontend/src/index.js
+++ src/dfx_main_frontend/src/index.js
@@ -1,19 +1,37 @@
 import { dfx_main_backend } from "../../declarations/dfx_main_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+window.addEventListener("load", async function(){
+  const current = await dfx_main_backend.checkBalance();
+  this.document.getElementById("value").innerText = Math.round(current * 100) / 100; 
+});
 
-  const name = document.getElementById("name").value.toString();
+document.querySelector("form").addEventListener("submit", async function(){
+  const inputAmount = parseFloat(document.getElementById("amount").value); 
+  const outputAmount = parseFloat(document.getElementById("amount2").value); 
+
+  const button = document.getElementById("submit")
+  const button2 = document.getElementById("submit2")
 
   button.setAttribute("disabled", true);
+  button2.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await dfx_main_backend.greet(name);
+  if ((document.getElementById("amount").value.length) != 0){
+    await dfx_main_backend.topUp(inputAmount);
+    await dfx_main_backend.compound();
+  };
+
+  if ((document.getElementById("amount2").value.length) != 0){
+    await dfx_main_backend.withdrawl(outputAmount);
+    await dfx_main_backend.compound();
+
+  };
+
+  const current = await dfx_main_backend.checkBalance();
+  document.getElementById("value").innerText = Math.round(current * 100) / 100;
+  document.getElementById("amount").value = "";
+  document.getElementById("amount2").value = "";
 
   button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+  button2.removeAttribute("disabled")
+  
 });
